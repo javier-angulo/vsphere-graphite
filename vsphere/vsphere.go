d58e1d4db18f96af3292d22fc2283696a0087c5d
Cedric Blomart <cedric.blomart@minfin.fed.be>
2017-06-03 22:27:56 +0200

    bad resourcepool reference modification

--- vsphere/vsphere.go
+++ vsphere/vsphere.go
@@ -317,10 +317,10 @@ func (vcenter *VCenter) Query(interval int, domain string, channel *chan []backe
 		if mor.Type == "ResourcePool" {
 			// find the full path of the resource pool
 			poolpath := ""
-			poolmor := mor
+			poolmor := &mor
 			ok := true
 			for ok {
-				poolname, ok := morToName[poolmor]
+				poolname, ok := morToName[*poolmor]
 				if !ok {
 					// could not find name
 					errlog.Println("Could not find name for resourcepool " + mor.String())
@@ -329,7 +329,7 @@ func (vcenter *VCenter) Query(interval int, domain string, channel *chan []backe
 				// add the name to the path
 				poolpath = poolname + "/" + poolpath
 				stdlog.Println("updated pool path: " + poolpath)
-				poolmor, ok := morToParent[poolmor]
+				poolmor, ok := morToParent[*poolmor]
 				if !ok {
 					// no parent pool found
 					errlog.Println("Could not find parent for resourcepool " + poolname)
