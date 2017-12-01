bf8bb4a75f9e7ed122f939c01f3cf4de0a465de4
BLOMART CEDRIC J.F <cedric.blomart@minfin.fed.be>
2017-12-01 22:11:02 +0100

    correct % disk usage

--- vsphere/vsphere.go
+++ vsphere/vsphere.go
@@ -658,7 +658,7 @@ func (vcenter *VCenter) Query(interval int, domain string, channel *chan backend
 					Counter:      "usage",
 					Instance:     diskPath,
 					Rollup:       "latest",
-					Value:        int64(1000 * (1 -  (float64(diskInfo.FreeSpace) / float64(diskInfo.Capacity)))),
+					Value:        int64(10000 * (1 -  (float64(diskInfo.FreeSpace) / float64(diskInfo.Capacity)))),
 					Datastore:    datastore,
 					ESXi:         vmhost,
 					Cluster:      cluster,
