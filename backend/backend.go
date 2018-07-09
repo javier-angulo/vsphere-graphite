fb10edfe4c1d1b14fb52cbbc6a0a8e30038eb3e9
Cédric Blomart <cblomart@gmail.com>
2018-07-10 00:20:45 +0200

    omit empty in json

--- backend/backend.go
+++ backend/backend.go
@@ -29,16 +29,16 @@ type Point struct {
 	Instance     string   `influx:"tag,instance"`
 	Rollup       string   `influx:"key,3"`
 	Value        int64    `influx:"value"`
-	Datastore    []string `influx:"tag,datastore"`
-	ESXi         string   `influx:"tag,host"`
-	Cluster      string   `influx:"tag,cluster"`
-	Network      []string `influx:"tag,network"`
-	ResourcePool string   `influx:"tag,resourcepool"`
-	Folder       string   `influx:"tag,folder"`
-	ViTags       []string `influx:"tag,vitags"`
-	NumCPU       int32    `influx:"tag,numcpu"`
-	MemorySizeMB int32    `influx:"tag,memorysizemb"`
-	Timestamp    int64    `influx:"time" elastic:"type:date,format:epoch_second"`
+	Datastore    []string `influx:"tag,datastore" json:",omitempty"`
+	ESXi         string   `influx:"tag,host" json:",omitempty"`
+	Cluster      string   `influx:"tag,cluster" json:",omitempty"`
+	Network      []string `influx:"tag,network" json:",omitempty"`
+	ResourcePool string   `influx:"tag,resourcepool" json:",omitempty"`
+	Folder       string   `influx:"tag,folder" json:",omitempty"`
+	ViTags       []string `influx:"tag,vitags" json:",omitempty"`
+	NumCPU       int32    `influx:"tag,numcpu" json:",omitempty"`
+	MemorySizeMB int32    `influx:"tag,memorysizemb" json:",omitempty"`
+	Timestamp    int64    `influx:"time" elastic:"type:date,format:epoch_second" json:",omitempty"`
 }
 
 // InfluxPoint is the representation of the parts of a point for influx
