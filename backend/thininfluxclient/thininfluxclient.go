e1e045ee71f88dedfd14f343c271e94b367c02db
Cédric Blomart <cblomart@gmail.com>
2018-08-03 13:58:45 +0200

    revert to std ffjson

--- backend/thininfluxclient/thininfluxclient.go
+++ backend/thininfluxclient/thininfluxclient.go
@@ -11,6 +11,8 @@ import (
 	"net/http"
 	"strconv"
 	"strings"
+
+	json "github.com/pquerna/ffjson/ffjson"
 )
 
 // constants
@@ -139,8 +141,7 @@ func (client *ThinInfluxClient) Send(lines []string) error {
 		if err != nil {
 			return err
 		}
-		err = jsonerr.UnmarshalJSON(body)
-		//err = json.Unmarshal(body, &jsonerr)
+		err = json.Unmarshal(body, &jsonerr)
 		if err != nil {
 			return err
 		}
