6248a4e77d62cee38bc3135eb2d1618182a64f30
Cédric BLOMART <cedric.blomart@economie.fgov.be>
2019-04-08 15:08:36 +0200

    no trim need and propert match test

--- vsphere-graphite.go
+++ vsphere-graphite.go
@@ -170,15 +170,13 @@ func (service *Service) Manage() (string, error) {
 	envvcenters := []*vsphere.VCenter{}
 	validvcenter := regexp.MustCompile(vcenterdefreg)
 	for _, e := range os.Environ() {
-		// trim e
-		kv := strings.Trim(e, " \r\n")
 		// check if a vcenter definition
 		if strings.HasPrefix(kv, "VCENTER_") {
-			if validvcenter.MatchString(kv) {
-				log.Printf("cannot parse vcenter: '%s'\n", kv)
+			if !validvcenter.MatchString(kv) {
+				log.Printf("cannot parse vcenter: '%s'\n", e)
 				continue
 			}
-			matches := validvcenter.FindStringSubmatch(kv)
+			matches := validvcenter.FindStringSubmatch(e)
 			names := validvcenter.SubexpNames()
 			username := ""
 			password := ""
@@ -197,15 +195,15 @@ func (service *Service) Manage() (string, error) {
 				}
 			}
 			if len(username) == 0 {
-				log.Printf("cannot find username in vcenter: '%s'", kv)
+				log.Printf("cannot find username in vcenter: '%s'", e)
 				continue
 			}
 			if len(password) == 0 {
-				log.Printf("cannot find password in vcenter: '%s'", kv)
+				log.Printf("cannot find password in vcenter: '%s'", e)
 				continue
 			}
 			if len(hostname) == 0 {
-				log.Printf("cannot find hostname in vcenter: '%s'", kv)
+				log.Printf("cannot find hostname in vcenter: '%s'", e)
 				continue
 			}
 			vcenter := vsphere.VCenter{
